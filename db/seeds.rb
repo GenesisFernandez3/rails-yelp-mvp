9586e297267edaf3e3f049d581c9136ea185a82a
genesis fernandez <gensdugarte@gmail.com>
2021-10-20 18:05:33 -0400

    Ejercicio Terminado

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+puts "Creating restaurants..."
+los_chorros = { name: 'Restaurant Los Chorros', address: 'Los chorros, Caracas', phone_number: 0212-3254, category: 'french' }
+pizzeria = { name: 'Pizzeria altamira', address: 'altamira, Caracas', phone_number: 0212-5241, category: 'italian' }
+chinese = { name: 'Chinese restaurant', address: 'chacao, Caracas', phone_number: 0212-5674, category: 'chinese' }
+japanese = { name: 'Restaurant Jp', address: 'Plaza Vzla, Caracas', phone_number: 0212-8745, category: 'japanese' }
+belgian = { name: 'Belgian Restaurant', address: 'Las mercedes, Caracas', phone_number: 0212-5874, category: 'belgian' }
+
+[los_chorros, pizzeria, chinese, japanese, belgian].each do |attributes|
+    restaurant = Restaurant.create!(attributes)
+    puts "Created #{restaurant.name}"
+end
+puts "Finished"
